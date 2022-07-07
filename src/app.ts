5f378e0b0e2f8af38f1b85fa53ec01b0d35940ff
Nirbose <nirbose2@gmail.com>
2022-07-07 19:30:15 +0200

    new setup

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,5 @@
-export * from './app.native.js'
+export * from "discord.js"
+export * from "./app.native.js"
+export * from "./namespaces/utils.native.js"
+export * as discord from "discord.js"
+export * as utils from "./namespaces/utils.native.js"
