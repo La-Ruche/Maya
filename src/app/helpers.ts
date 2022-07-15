6a656755ed47346c57319eaa3be7eed19a611ac7
Nirbose <nirbose2@gmail.com>
2022-07-15 18:15:01 +0200

    add subs function in helpers

--- src/app/helpers.ts
+++ src/app/helpers.ts
@@ -1,6 +1,7 @@
 import { load } from 'js-yaml'
 import fs from 'fs';
 import path from 'path';
+import { Command, CommandMessageType, SlashType } from './command';
 
 export async function config(name: string) {
     const file: any[] = load(
@@ -8,4 +9,10 @@ export async function config(name: string) {
     ) as any[]
 
     return file[name as any]
+}
+
+export async function subs(name: string) {
+    let file = await import(path.join(process.cwd(), 'dist', 'commands', 'subs', `${name}.js`))
+
+    return file.default as Command<keyof CommandMessageType, SlashType>
 }
--- src/commands/make.ts
+++ src/commands/make.ts
@@ -22,33 +22,6 @@ export default new app.Command({
         message.send('reply')
     },
     subs: [
-        new app.Command({
-            name: 'select-role',
-            only: 'SLASH',
-            description: 'Make react role function in this server',
-            userPermissions: ['ADMINISTRATOR'],
-            async run(message) {
-                const row = new app.MessageActionRow()
-                    .addComponents(
-                        new app.MessageSelectMenu()
-                            .setCustomId('select-role')
-                            .setMaxValues(1)
-                            .setMinValues(1)
-                            .setPlaceholder("Select your roles !")
-                            .setOptions([
-                                {
-                                    label: "t1",
-                                    description: "role t1",
-                                    value: "11111"
-                                }
-                            ])
-                    )
-
-                let member = message.member as app.GuildMember
-                let channel = await member.guild.channels.fetch(message.channelId)
-
-                if (channel?.isText()) channel.send({ content: "test", components: [row] })
-            }
-        })
+        await app.subs('select-role')
     ]
 })
